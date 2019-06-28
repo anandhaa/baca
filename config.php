b7c49de6d98d9d8269c6dec6b5e87e0173e5902e
anandhaa <52008534+anandhaa@users.noreply.github.com>
2019-06-29 05:44:43 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
 
-$uid="Facebook_xxxxxxxxxxxxxxxxxxx";
-$raw_uid="xxxx-xxxx-xxxx-xxxx-xxxx";
-$user_name="User+name";
-$invite_code="xxxxxxx";
-$user_token="xxxxxxxxx";
-$own_mac="02%xxxx%xxxx%xxxx%xxxx%xxxx";
-$android_id="xxxxxxxxxxxx";
-$device_type="xxxx++xxxx";
+$uid="Facebook_2194752097448360";
+$raw_uid="38f820ff-8062-41d3-b18f-371c37d6c513";
+$user_name="Anandha+Bakhti";
+$invite_code="FA3DqAXlXM";
+$user_token="AbxfMxsLCej6Aan6";
+$own_mac="02%3A00%3A00%3A00%3A00%3A00";
+$android_id="7bbdf8096ab5bad1";
+$device_type="Xiaomi++Redmi+5+Plus";
 
 
 ?>
